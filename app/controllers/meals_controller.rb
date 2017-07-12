c34e0bff8d621940129151c1cc5ed1a2a70d3223
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-12 12:42:02 -0400

    stubbing out create method

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,32 @@
 class MealsController < ApplicationController
 
   def new
-    
+    @meal = Meal.new
+    @ingredients = Ingredient.all
   end
 
+  def create
+    @meal = Meal.new(meal_params)
+    binding.pry
+    # @new_ingredients = params[:ingredients][:name]
+
+    
+
+    if @meal.save
+      binding.pry
+    else
+      
+    end
+  end  
+
+  private
+
+  def set_meal
+
+  end
+  
+  def meal_params
+    params.require(:meal).permit(:meal_type, :description, ingredient_ids: [], ingredients_attributes: [:name])
+  end
+  
 end
