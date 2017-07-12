fc5efeba012b8744935ddd6cd8b4d6d21d6faab0
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-12 17:54:58 -0400

    troubleshooting blank attributes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,16 +6,24 @@ class Meal < ApplicationRecord
   validates_presence_of :meal_type, :description
   validates :description, length: { maximum: 30 }
 
-  accepts_nested_attributes_for :ingredients
-
+  accepts_nested_attributes_for :ingredients, reject_if: lambda {|attributes| attributes['name'].blank?}
+ 
   def parse_ingredients(new_ingredients)
     new_ingredients.split(',').each do |ingredient|
-      ingredients << Ingredient.find_or_create_by(name: ingredient.strip)
+      ingredients << Ingredient.find_or_create_by(name: ingredient.strip.downcase)
     end
   end
 
-  # def ingredients_attributes=(ingredients)
+  def ingredients_attributes=(ingredients_attributes)
     # ingredients = [{name: "sausage"}, {name: "eggs"}, {name: "cheese"}]
     # self.ingredients.find_or_create_by
-  # end
+    new_ingredients = ingredients_attributes.dig('0', 'name')
+    
+    if new_ingredients.include?(',')
+      parse_ingredients(new_ingredients)
+    elsif !new_ingredients.blank?
+      ingredient = Ingredient.find_or_create_by(ingredients_attributes)
+      self.ingredients << ingredient
+    end
+  end
 end
