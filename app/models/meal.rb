e6a772254da4637c89ecc8e803a01d6332829080
Mike Etlinger <mk-etlinger@gmail.com>
2017-07-12 20:48:51 -0400

    added rejects empty attributes for ingredients_attributes=

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,7 +5,6 @@ class Meal < ApplicationRecord
 
   validates_presence_of :meal_type, :description
   validates :description, length: { maximum: 30 }
-
  
   def parse_ingredients(new_ingredients)
     new_ingredients.split(',').each do |ingredient|
@@ -14,13 +13,11 @@ class Meal < ApplicationRecord
   end
 
   def ingredients_attributes=(ingredients_attributes)
-    # ingredients = [{name: "sausage"}, {name: "eggs"}, {name: "cheese"}]
-    # self.ingredients.find_or_create_by
+    return if ingredients_attributes.dig('0', 'name')
     new_ingredients = ingredients_attributes.dig('0', 'name')
-    binding.pry
     if new_ingredients.include?(',')
       parse_ingredients(new_ingredients)
-    elsif !new_ingredients.blank?
+    else
       ingredient = Ingredient.find_or_create_by(ingredients_attributes)
       self.ingredients << ingredient
     end
