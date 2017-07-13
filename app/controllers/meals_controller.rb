0f4726c47a9bcb83a173f1e513e33469bd946165
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-12 23:13:08 -0400

    added delete action and built set_meal method

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -7,24 +7,32 @@ class MealsController < ApplicationController
 
   def create
     @meal = current_user.meals.build(meal_params)
-    # @new_ingredients = params[:ingredients][:name]
 
-    
-
-    binding.pry
     if @meal.save
       redirect_to dashboard_path
     else
       
     end
+  end
+
+  def show
+    set_meal
   end  
+
+  def destroy
+    set_meal
+    binding.pry
+    @meal.ingredients.delete
+    @meal.destroy
+    redirect_to dashboard_path
+  end
 
   private
 
   def set_meal
-
+    @meal = Meal.find_by(id: params[:id])
   end
-  
+
   def meal_params
     params.require(:meal).permit(:meal_type, :description, ingredient_ids: [], ingredients_attributes: [:name])
   end
