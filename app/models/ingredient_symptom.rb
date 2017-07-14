7dae3c0d6558b396d1a63440966d8d52974fb807
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-14 10:20:43 -0400

    updated schema and migrations

--- app/models/ingredient_symptom.rb
+++
@@ -1,4 +0,0 @@
-class IngredientSymptom < ApplicationRecord
-  belongs_to :ingredient
-  belongs_to :symptom
-end
--- db/migrate/20170713004506_add_quantity_to_meal_ingredients.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityToMealIngredients < ActiveRecord::Migration[5.1]
-  def change
-    add_column :meal_ingredients, :quantity, :string
-  end
-end
