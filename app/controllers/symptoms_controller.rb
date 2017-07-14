5ca3e0df620752796f028ab94a42dab7add04bc4
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-14 19:09:05 -0400

    troubleshooting nested forms

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -5,13 +5,22 @@ class SymptomsController < ApplicationController
   end
   
   def new
-    @ingredients = Ingredient.joins(:meals).where(meals: { user_id: current_user.id })
-    binding.pry
     @symptom = Symptom.new
+    @reaction_log = ReactionLog.new
   end
   
   def create
+    binding.pry
+  end
 
+  private
+
+  def set_symptom
+    @symptom = Symptom.find_by(id: params[:id])
+  end
+  
+  def symptom_params
+    params.require(:symptom).permit(:description, :reaction_log[:severity][])
   end
   
 end
