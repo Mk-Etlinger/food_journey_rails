c4c0deee752a43eb1fc2183a7077835f5749bdd5
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-14 19:05:08 -0400

    updated schema and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714135725) do
+ActiveRecord::Schema.define(version: 20170714161739) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 20170714135725) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
