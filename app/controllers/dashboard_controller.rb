e139f68717a6c78ff2d959ed263b83b81a515333
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-17 08:51:34 -0400

    added recent symptoms to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   def index
     user_signed_in?
     authenticate_user!
-    @meals = current_user.meals.limit(5)
+    @meals = current_user.meals.limit(4)
+    @symptoms = current_user.symptoms.limit(3)
   end
 end
