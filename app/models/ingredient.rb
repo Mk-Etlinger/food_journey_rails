c7dc646f88df587f13874af6de17bde5f7041e54
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-17 16:03:04 -0400

    added reminder

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,7 @@ class Ingredient < ApplicationRecord
   has_many :meal_ingredients
   has_many :meals, through: :meal_ingredients
   has_many :reactions
-  has_many :symptoms, through: :reactions
+  has_many :symptoms, through: :reactions # add unique: true?
 
   validates_presence_of :name
   validates :name, format: { with: /[a-zA-Z,]/ }
