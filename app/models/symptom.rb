c6dd1918f580162643d7faa41224f9bf1e291565
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-17 08:55:49 -0400

    added distinct ingredients and has_many through: reactions

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,15 +1,12 @@
 class Symptom < ApplicationRecord
   belongs_to :user
   has_many :reactions
-  has_many :ingredients, through: :reactions
-  has_many :reaction_logs
+  has_many :ingredients, -> { distinct }, through: :reactions
+  has_many :reaction_logs, through: :reactions
 
   def reactions_attributes=(reaction_attributes)
     reaction_attributes.values.each do |attribute|
-      binding.pry
-      # you have to iterate over reactions? and find the associated reaction
       self.reactions.each do |reaction|
-        binding.pry
         reaction.update(attribute)
       end
     end
@@ -17,34 +14,26 @@ class Symptom < ApplicationRecord
   end
 
   def ingredients_attributes=(attr)
+    return if self.persisted?
     self.user_id = attr.dig('0', 'current_user_id')
-    meals = Meal.for_user(user).where("created_at > ?",(Time.now - 3.day))
+    meals = Meal.for_user(user).created_after((Time.now - 3.day))
+    binding.pry
     meals.each do |meal|
       meal.ingredients.each do |ingredient|
-        binding.pry
         self.ingredients << ingredient
+        binding.pry
       end
     end
     save
   end  
 
-  # def reaction_logs=(attributes)
-  #   self.reactions.each do |reaction|
-  #     reaction.reaction_logs.create(attributes)
-  #   end
-  # end
-
-  # def associate_ingredients_attributes
-  #   meals = Meal.for_user(self.user).where("created_at > ?",(Time.now - 3.day))
-  #   meals.each { |meal| self.ingredients << meal.ingredients }
-  #  meals = Meal.for_user(self.user).where("created_at > ?",(Time.now - 3.day))
-  #   meals.each do |meal|
-  #     meal.ingredients.each do |ingredient|
-  #       binding.pry
-  #       self.ingredients << ingredient
-  #       self.reactions.build(reaction_attributes['0'])
-  #     end
-  #   end
-  # end
+  def reaction_logs=(attributes)
+    hours_ago = attributes['occurred_at'].to_i
+    increment = attributes['increment']
+    attributes['occurred_at'] = Time.now - hours_ago.to_i.hour
+    self.reactions.each do |reaction|
+      reaction.reaction_logs.create(attributes)
+    end
+  end
 
 end
