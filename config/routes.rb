83c1239a9960b76258f8bec112181aec59e5778a
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-17 14:11:12 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   
   resources :symptoms
 
-  get '/users/:id/meals/new', to: 'meals#new'
+  get '/users/:id/meals/new', to: 'meals#new', as: 'nested_new_meal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
