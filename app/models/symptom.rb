f18b9f564985605e410480ecc14176c5355098bd
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-17 13:20:36 -0400

    cleaned up prys and added comments

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -7,30 +7,30 @@ class Symptom < ApplicationRecord
   def reactions_attributes=(reaction_attributes)
     reaction_attributes.values.each do |attribute|
       self.reactions.each do |reaction|
+        binding.pry
         reaction.update(attribute)
       end
     end
   
   end
 
+  # still need to make associations based on dynamic entry of elapsed time
   def ingredients_attributes=(attr)
-    return if self.persisted?
+    return if self.persisted? # currently not allowing this to update
     self.user_id = attr.dig('0', 'current_user_id')
     meals = Meal.for_user(user).created_after((Time.now - 3.day))
-    binding.pry
     meals.each do |meal|
       meal.ingredients.each do |ingredient|
         self.ingredients << ingredient
-        binding.pry
       end
     end
-    save
-  end  
+    # save
+  end
 
   def reaction_logs=(attributes)
     hours_ago = attributes['occurred_at'].to_i
-    increment = attributes['increment']
-    attributes['occurred_at'] = Time.now - hours_ago.to_i.hour
+    # increment = attributes['increment']
+    attributes['occurred_at'] = Time.now - hours_ago.hour
     self.reactions.each do |reaction|
       reaction.reaction_logs.create(attributes)
     end
