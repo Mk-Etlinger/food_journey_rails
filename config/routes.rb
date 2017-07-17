05894c92a982bf22f951bd9fee447732c3a24a86
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-17 08:58:43 -0400

    properly nested ingredients route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root 'application#index'
   get 'dashboard', to: 'dashboard#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :meals
-  resources :ingredients
+  resources :meals do
+    resources :ingredients, only: [:index]
+  end
+  
   resources :symptoms
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
