0c14a83e53b826a516e25db6586b42cda87a33c4
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-19 18:00:15 -0400

    added a valid meals method used to redirect from symptoms

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -8,10 +8,10 @@ class SymptomsController < ApplicationController
   
   def new
     @symptom = Symptom.new
-    @reaction_log = ReactionLog.new
   end
   
   def create
+    redirect_to nested_new_meal_path(current_user), alert: no_meals_message and return if valid_meals.empty?
     @symptom = current_user.symptoms.build(symptom_params)
 
     if @symptom.save
@@ -37,7 +37,7 @@ class SymptomsController < ApplicationController
       render :edit
     end
   end
-  # test destroy to make sure ingredients remain in db(do associations remain?)
+
   def destroy
     if user_authorized?
       @symptom.ingredients.clear
@@ -56,14 +56,23 @@ class SymptomsController < ApplicationController
 
    def symptom_params
     params.require(:symptom).permit(:description,
-                                    ingredients_attributes: [:current_user_id, :hours_elapsed],
+                                    ingredients_attributes: [:current_user_id, :hours_elapsed, :occurred_at],
                                     reactions_attributes: [:severity, :stress_level, :notes],
-                                    reaction_logs: [:occurred_at]
-                                    )
+                                    reaction_logs: [:occurred_at])
   end
 
   def user_authorized?
     @symptom.user == current_user
+  end
+
+  def valid_meals
+    hours = symptom_params['ingredients_attributes']['0']['occurred_at'].to_f
+    occurred_at = Time.current.ago(hours.hour)
+    Meal.for_user(current_user).created_within(occurred_at - 3.day, occurred_at)
+  end
+
+  def no_meals_message
+    "Please create a meal first. Currently you have no meals within the selected time period"
   end
 
 end
