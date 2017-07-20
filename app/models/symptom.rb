a1b8894447ad4e3cabe4a787b2604941ae10cce7
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-20 11:29:02 -0400

    refactored ingredients_attributes=

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -14,14 +14,11 @@ class Symptom < ApplicationRecord
     end
   end
 
-  # still need to make associations based on dynamic entry of elapsed time
   def ingredients_attributes=(attributes)
     return if self.persisted? # currently not allowing this to update
-    set_user_id(attributes)
-    three_days = set_occurred_at(attributes) - 3.day
-    binding.pry
-    meals = Meal.for_user(user).created_within(three_days, @occurred_at)
-    meals.each do |meal|
+    set_user_id(attributes) && set_occurred_at(attributes)
+    set_meals_within_three_days
+    @meals.each do |meal|
       meal.ingredients.each do |ingredient|
         self.ingredients << ingredient
       end
@@ -30,10 +27,13 @@ class Symptom < ApplicationRecord
 
   def reaction_logs=(attributes)
     attributes['occurred_at'] = @occurred_at
-    binding.pry
     self.reactions.each do |reaction|
       reaction.reaction_logs.create(attributes)
     end
+  end
+
+  def set_meals_within_three_days
+    @meals = Meal.for_user(user).created_within(@occurred_at - 3.day, @occurred_at)
   end
 
   def set_occurred_at(attributes)
@@ -44,5 +44,4 @@ class Symptom < ApplicationRecord
   def set_user_id(attributes)
     self.user_id = attributes.dig('0', 'current_user_id')
   end
-
 end
