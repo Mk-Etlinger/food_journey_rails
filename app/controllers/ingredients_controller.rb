a84343f5ae9855dd1c4bbd7089cccd20eb85a967
ETphonehomeslice <mk.etlinger@gmail.com>
2017-07-25 16:59:15 -0400

    implemented rough safe ingredient feature

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,5 +5,25 @@ class IngredientsController < ApplicationController
     authenticate_user!
     @meal = Meal.find_by(id: params[:meal_id])
   end
+
+  def safe
+    @ingredient = Ingredient.new
+  end
+
+  def mark_safe
+    safe_ingredients = ingredient_params[:id]
+    @ingredients = Ingredient.where(id: safe_ingredients)
+    @ingredients.each { |ingredient| ingredient.update(safe: true) }
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(id: [])
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find_by(id: params[])
+  end
   
 end
